c757fc12ee6c765f34221aa23b21a0b40adc1843
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-04-09 22:41:36 +0000

    created in python a button

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,48 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Jet Lu
+Created on: Apr 2026
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+# start
+display.show(Image.HAPPY)
+
+# variables
+loopCounterY= 0
+loopCounterX= 0
+
+while True :
+    if button_a.was_pressed() :
+        display.clear()
+        loopCounterY= 0
+        loopCounterX= 0
+
+        # move pixel
+        while (loopCounterX <= 4) :
+            display.set_pixel(loopCounterX, loopCounterY, 9)
+            loopCounterX += 1
+            sleep(500)
+            display.set_pixel(loopCounterX, loopCounterY, 0)
+
+        # move pixel down
+        while (loopCounterY <= 4) :
+            display.set_pixel(loopCounterX, loopCounterY, 9)
+            loopCounterY += 1
+            sleep(500)
+            display.set_pixel(loopCounterX, loopCounterY, 0)
+
+        # move left
+        while (loopCounterX >= 0) :
+            display.set_pixel(loopCounterX, loopCounterY, 9)
+            loopCounterX -= 1
+            sleep(500)
+            display.set_pixel(loopCounterX, loopCounterY, 0)
+
+        # move up
+        while (loopCounterY >= 0) :
+            display.set_pixel(loopCounterX, loopCounterY, 9)
+            loopCounterY -= 1
+            sleep(500)
+            display.set_pixel(loopCounterX, loopCounterY, 0)
