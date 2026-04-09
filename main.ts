1acc9b7be7737ed26dbfefe566c300c049b25797
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-04-09 13:24:08 -0400

    Please

--- main.ts
+++ main.ts
@@ -1,8 +1,82 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jet Lu
+ * Created on: Apr 2026
+ * This program makes a sprite go around the screen.
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Happy)
+
+// variables
+let loopCounterX = 0
+let loopCounterY = 0
+let sprite: game.LedSprite = null
+
+// button a
+input.onButtonPressed(Button.A,  function () {
+  // setup
+  basic.clearScreen()
+  loopCounterX = 0
+  loopCounterY = 0
+  sprite = game.createSprite(0, 0)
+
+  // move
+  while (loopCounterX <= 4) {
+    sprite.set(LedSpriteProperty.X, loopCounterX)
+    loopCounterX += 1
+    basic.pause(500)
+  }
+  while (loopCounterY <= 4) {
+    sprite.set(LedSpriteProperty.Y, loopCounterY)
+    loopCounterY += 1
+    basic.pause(500)
+  }
+  while (loopCounterX >= 0) {
+    sprite.set(LedSpriteProperty.X, loopCounterX)
+    loopCounterX -= 1
+    basic.pause(500)
+    }
+  while (loopCounterY >= 0) {
+    sprite.set(LedSpriteProperty.Y, loopCounterY)
+    loopCounterY -= 1
+    basic.pause(500)
+  }
+  sprite.delete()
+  basic.pause(500)
+  basic.showIcon(IconNames.Happy)
+})
+
+// button b
+input.onButtonPressed(Button.B, function () {
+  // setup
+  basic.clearScreen()
+  loopCounterX = 0
+  loopCounterY = 0
+  sprite = game.createSprite(0, 0)
+
+  // move
+  while (loopCounterY <= 4) {
+    sprite.set(LedSpriteProperty.Y, loopCounterY)
+    loopCounterY += 1
+    basic.pause(500)
+  }
+  while (loopCounterX <= 4) {
+    sprite.set(LedSpriteProperty.X, loopCounterX)
+    loopCounterX += 1
+    basic.pause(500)
+  }
+  while (loopCounterY >= 0) {
+    sprite.set(LedSpriteProperty.Y, loopCounterY)
+    loopCounterY -= 1
+    basic.pause(500)
+  }
+  while (loopCounterX >= 0) {
+    sprite.set(LedSpriteProperty.X, loopCounterX)
+    loopCounterX -= 1
+    basic.pause(500)
+  }
+  sprite.delete()
+  basic.pause(500)
+  basic.showIcon(IconNames.Happy)
+})
